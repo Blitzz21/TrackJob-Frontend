ec74bfbc341120e226ee4ee07e37d34d7dba9982
Blitz <johnfloydmarticio@proton.me>
2025-10-01 12:41:12 +0800

    Minor fix: added https on baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "httpss://trackjob-backend.onrender.com/api",
+  baseURL: "https://trackjob-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {

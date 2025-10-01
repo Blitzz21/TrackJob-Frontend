a928e8b1977391c78e1b4b46a1bac7e2cd43b7b7
Blitz <johnfloydmarticio@proton.me>
2025-10-01 12:25:28 +0800

    Minor fix: fixed the baseURL for api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://https://trackjob-backend.onrender.com//api",
+  baseURL: "http://trackjob-backend.onrender.com//api",
 });
 
 api.interceptors.request.use((config) => {
